e513aa6c161fffac1258235ffa844464bfdded66
Keerthi612 <157579500+Keerthi612@users.noreply.github.com>
2026-02-17 15:11:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,141 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import dexie from "dexie";
 
-// component route endpoints
-import List from '../components/List'
-import CSV from '../components/CSV'
-import Incident from '../components/Incident'
-import IncidentEdit from '../components/IncidentEdit'
+Vue.use(Vuex);
 
-Vue.use(VueRouter);
+// Setup our IndexedDB database
+let incidentsDB = new dexie('IncidentsDB');
+incidentsDB.version(1).stores({
+  incidents: '++id,name,discovered,description,departments',
+});
+Vuex.Store.prototype.$db = incidentsDB;
 
-export default new VueRouter({
-  routes:[
-    // Component Routes
-    {
-      path:'/',
-      redirect:'/list'
-    },{
-      path:'/list',
-      name:'list',
-      component: List
-    },{
-      path:'/csv',
-      name:'csv',
-      component: CSV
-    },{
-      path:'/incident/:incident_id',
-      name:'incident',
-      component: Incident
-    },{
-      path:'/incident-new',
-      name:'incident-new',
-      component: IncidentEdit
-    },{
-      path:'/incident-edit/:incident_id',
-      name:'incident-edit',
-      component: IncidentEdit
+// Create Store
+
+export default new Vuex.Store({
+  strict: true,
+  state: {
+    incidents: [],
+    sortedIncidents: [],
+    activeIncident: null,
+    incidentsLoaded: false
+  },
+  actions: {
+
+    getIncidents({commit}) {
+      return this.$db.incidents
+        .reverse() // default sort on most recently added incident
+        .toArray()
+        .then(incidents => {
+          return commit('setIncidents', incidents);
+        })
+    },
+
+    getIncident({commit}, incident_id) {
+      return this.$db.incidents
+        .get({'id':Number(incident_id)})
+        .then((incident) => {
+          return commit('setActiveIncident', incident);
+        })
+    },
+
+    setActiveIncident({commit}, incident) {
+      return commit('setActiveIncident', incident);
+    },
+    clearActiveIncident({commit}) {
+      return commit('clearActiveIncident');
+    },
+
+    addIncident({commit}, incident) {
+      return this.$db.incidents
+        .add(incident)
+        .then(id => {
+          incident.id = id;
+          commit('addIncident', incident);
+          return commit('setActiveIncident', incident)
+        })
+        .catch(err => {throw(err)})
+    },
+
+    updateIncident({commit}, incident) {
+      return this.$db.incidents
+        .put(incident)
+        .then(() => {
+          return commit('updateIncident', incident)
+        })
+        .catch(err => {throw(err)})
+    },
+
+    setSortedIncidents({commit}, sortedIncidents) {
+      return commit('setSortedIncidents', sortedIncidents);
+    },
+
+    deleteIncident({commit}, id) {
+      return this.$db.incidents
+        .delete(id)
+        .then(() => {
+          return commit('deleteIncident', id)
+        })
+        .catch(err => {throw(err)})
+    },
+
+    deleteActiveIncident({commit, state}) {
+      let id = state.activeIncident.id;
+      if (id) {
+        return this.$db.incidents
+          .delete(id)
+          .then(() => {
+            commit('clearActiveIncident');
+            return commit('deleteIncident', id)
+          })
+          .catch(err => {throw(err)})
+      } else {
+        throw({message:'Could not find active incident id'})
+      }
     }
-    // Route Error
-    ,{
-      path:'*',
-      redirect:'/list'
+
+  },
+  mutations: {
+
+    setIncidents(state, incidents) {
+      state.incidentsLoaded = true;
+      state.incidents = incidents;
+    },
+
+    setActiveIncident(state, incident) {
+      state.activeIncident = incident;
+    },
+
+    clearActiveIncident(state) {
+      state.activeIncident = null;
+    },
+
+    addIncident(state, incident) {
+      state.incidents.unshift(incident);
+    },
+
+    updateIncident(state, incident) {
+      let i = state.incidents.findIndex(inc => inc.id === incident.id);
+      if (i > -1) {
+        state.incidents[i] = incident;
+      } else {
+        throw({message:'Could not find incident by id ['+incident.id+']'})
+      }
+    },
+
+    setSortedIncidents(state, sortedIncidents) {
+      state.sortedIncidents = sortedIncidents;
+    },
+
+    deleteIncident(state, id) {
+      let i = state.incidents.findIndex(inc => inc.id === id);
+      if (i > -1) {
+        state.incidents.splice(i, 1);
+      } else {
+        throw({message:'Could not find incident by id ['+id+']'})
+      }
     }
-  ]
+
+  }
 })
